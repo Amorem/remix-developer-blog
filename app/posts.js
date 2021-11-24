ee9e179d8b47b168b68b3071ddb90f93b4a676a3
Cedric <cedric@pop-os.localdomain>
2021-11-24 18:03:58 +0100

    refactor

---
+++ app/posts.js
@@ -0,0 +1,8 @@
+export default function getPosts() {
+  let posts = [
+    { slug: "post-1", title: "Post 1" },
+    { slug: "post-2", title: "Post 2" },
+    { slug: "post-3", title: "Post 3" },
+  ];
+  return posts;
+}
--- app/routes/posts/index.jsx
+++ app/routes/posts/index.jsx
@@ -1,7 +1,19 @@
+import { useLoaderData } from "remix";
+import getPosts from "~/posts";
+
+export let loader = () => {
+  return getPosts();
+}
+
 export default function Posts() {
+  let posts = useLoaderData();
+  console.log(posts);
   return (
     <div>
       <h1>Posts</h1>
+      <ul>
+        {posts.map(post => (<li key={post.slug}>{post.title}</li>))}
+      </ul>
     </div>
   );
 }
